9da676791b9937e3195ea71d565c3842b7ae9734
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-24 09:25:15 +0800

    Delete handlers.py

--- handlers.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from tornado.web import RequestHandler
-from tornado.gen import coroutine
-
-
-class IndexHandler(RequestHandler):
-    @coroutine
-    def get(self):
-        self.render("index.html")
